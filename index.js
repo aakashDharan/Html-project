6452c0846b7bcc14bb17e35f71d8a8f7d4ce86fb
aakashDharan <akashdharan@gmail.com>
2024-07-13 21:00:56 +0530

    slider Added

--- index.js
+++ index.js
@@ -107,3 +107,13 @@ function isInView(element){
     );
 }
 
+// Slider Movement
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter>5){
+        counter = 1;
+    }
+}, 5000);
